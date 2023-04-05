2c22f2e14ad0c6d68a52ac75215003b5f3bdea81
ryu14110 <119416694+ryu14110@users.noreply.github.com>
2023-04-05 23:58:53 +0900

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-const colors = ["green","red","rgba(133,122,200)","#15025"];
-const btn = document.getElementById("btn");
-const color = document.querySelector(".color");
-
-btn.addEventListener("click", function() {
-  console.log(document.body);
-})
